c3bfe3d2732b304dadb9c6833c5c1bb3da6286a4
Alex Frost <zufrost.a@gmail.com>
2020-04-14 12:02:38 +0300

    Renew readme

--- src/com/zufrost/learn/HelloWorld1.java
+++ src/com/zufrost/learn/HelloWorld1.java
@@ -3,5 +3,6 @@ package com.zufrost.learn;
 public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World 3");
     }
 }
